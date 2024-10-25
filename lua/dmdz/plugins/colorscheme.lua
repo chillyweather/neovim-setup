944d0af264e8be06839acc1e8b905094dd0c6055
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-10-25 18:33:25 +0300

    update

--- lua/dmdz/plugins/colorscheme.lua
+++ lua/dmdz/plugins/colorscheme.lua
@@ -2,13 +2,61 @@
 return {
     {
         'neanias/everforest-nvim',
+        version = false,
+        lazy = false,
         priority = 1000, -- Make sure to load this before all other start plugins
         config = function()
-            -- Set the color scheme to everforest
-            vim.cmd 'colorscheme everforest'
+            --vim.cmd 'colorscheme everforest'
 
-            -- Optional: Additional configuration for the theme
-            -- Example: vim.g.everforest_background = 'soft'
+            vim.g.everforest_background = 'hard'
+        end,
+    },
+    {
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        config = function()
+            require('github-theme').setup {
+                -- ...
+            }
+        end,
+    },
+    {
+        'gregsexton/Atom',
+        config = function()
+            -- Apply the Lucario colorscheme
+            -- vim.cmd.colorscheme 'atom'
+        end,
+    },
+    {
+        'dmcgrady/vim-lucario',
+        config = function()
+            -- Apply the Lucario colorscheme
+            vim.cmd.colorscheme 'lucario'
+        end,
+    },
+    {
+        'ajmwagar/vim-deus',
+        config = function()
+            -- vim.cmd.colorscheme 'deus'
+        end,
+        opts = {},
+    },
+    {
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        'rebelot/kanagawa.nvim',
+        name = 'kanagawa',
+        config = function()
+            require('kanagawa').setup {
+                background = { -- map the value of 'background' option to a theme
+                    dark = 'wave', -- try "dragon" !
+                    light = 'lotus',
+                },
+            }
         end,
     },
 }
