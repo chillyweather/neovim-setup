f3de98fe8ddff44929a906e57c4a1368cdcddd5d
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-04-26 15:39:20 +0300

    update

--- lua/dmdz/plugins/vimBeGood.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'ThePrimeagen/vim-be-good',
-    lazy = false, -- load on startup (optional)
-    config = function()
-        -- Optional keymaps
-        vim.keymap.set('n', '<leader>vb', ':VimBeGood<CR>', { desc = 'Start VimBeGood' })
-    end,
-}
